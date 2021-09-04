a869d4ae37d785b226ccf106d3cecf8d9c4757c0
Ramona Meunier <gramyy7@gmail.com>
2021-09-04 22:20:32 +0200

    trying to fix imports

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/js/bootstrap.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'three/build/three.min';
 import 'vanta/src/vanta.waves';
-// import './main';
+import { scrollWin, fullPageScrolling } from "./scroll";
 import { effect } from "./vanta_effect";
 
 effect;
