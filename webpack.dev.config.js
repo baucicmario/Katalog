42adec214ea1b3322d7a6a037b587a8cfa694ef7
Ramona Meunier <gramyy7@gmail.com>
2021-09-05 19:59:05 +0200

    adding dev server config

---
+++ webpack.dev.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './resources/js/app.js',
+    output: {
+        filename: "build.js",
+        path: path.resolve(__dirname, 'dist'),
+        // publicPath: "/dist"
+    },
+    optimization: {
+        minimize: true
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, '/'),
+        },
+        compress: true,
+        port: 8080,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js%/i,
+                use: ['babel-loader']
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+            },
+            {
+                test: /.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: '../../fonts'
+                    }
+                }]
+            },
+            {
+                test: /\.(jpg|png)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: '../../img'
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+        }),
+        new MiniCssExtractPlugin({
+            filename: '../../css/dist/appstyle.css',
+        }),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ]
+}
