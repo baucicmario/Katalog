3e15ad7ec6be7d36454c3f3094ce97e923e55b9f
baucicmario <baucicmario2.6@gmail.com>
2021-09-29 22:53:29 +0200

    mobile version started

--- resources/js/multiscroll.js
+++ resources/js/multiscroll.js
@@ -1,39 +1,116 @@
+
 $(document).ready(function () {
-    $('#multiscroll').multiscroll({
+    //small screen right side removal
+    if ($(window).width() > $(window).height()) {
+
+        $('#mobile-left').addClass('d-none');
+        $('#mobile-left').removeClass('ms-left');
+        $('#mobile-right').addClass('d-none');
+        $('#mobile-right').removeClass('ms-right');
+
+
+        $('#large-left').removeClass('d-none');
+        $('#large-left').addClass('ms-left');
+        $('#large-right').removeClass('d-none');
+        $('#large-right').addClass('ms-right');
+
+
+
+    } else {
+
+        $('#large-left').addClass('d-none');
+        $('#large-left').removeClass('ms-left');
+        $('#large-right').addClass('d-none');
+        $('#large-right').removeClass('ms-right');
+
+
+        $('#mobile-left').removeClass('d-none');
+        $('#mobile-left').addClass('ms-left');
+        $('#mobile-right').removeClass('d-none');
+        $('#mobile-right').addClass('ms-right');
+    }
+
+    //medium screen scaling
+
+    if ($(window).width() > 1600) {
+        //remove class
+        $('#large-pelda1rightId').removeClass('medium-large-pelda1rightId');
+        $('#pelda1leftId').removeClass('medium-pelda1leftId');
+        $('#large-pelda2rightId').removeClass('medium-large-pelda2rightId');
+        $('#pelda2leftId').removeClass('medium-pelda2leftId');
+        $('#pelda3leftId').removeClass('medium-pelda3leftId');
+        $('#large-pelda3rightId').removeClass('medium-large-pelda3rightId');
+        $('#pelda4leftId').removeClass('medium-pelda4leftId');
+        $('#large-pelda4rightId').removeClass('medium-large-pelda4rightId');
+        $('#large-pelda5rightId').removeClass('medium-large-pelda5rightId');
+        $('#pelda5leftId').removeClass('medium-pelda5leftId');
+        $('#large-pelda6rightId').removeClass('medium-large-pelda6rightId');
+        $('#pelda6leftId').removeClass('medium-pelda6leftId');
+        $('#large-pelda7rightId').removeClass('medium-large-pelda7rightId');
+        $('#pelda7leftId').removeClass('medium-pelda7leftId');
+        //add classes
+        $('#large-pelda1rightId').addClass('large-pelda1rightId');
+        $('#pelda1leftId').addClass('pelda1leftId');
+        $('#large-pelda2rightId').addClass('large-pelda2rightId');
+        $('#pelda2leftId').addClass('pelda2leftId');
+        $('#pelda3leftId').addClass('pelda3leftId');
+        $('#large-pelda3rightId').addClass('large-pelda3rightId');
+        $('#pelda4leftId').addClass('pelda4leftId');
+        $('#large-pelda4rightId').addClass('large-pelda4rightId');
+        $('#large-pelda5rightId').addClass('large-pelda5rightId');
+        $('#pelda5leftId').addClass('pelda5leftId');
+        $('#large-pelda6rightId').addClass('large-pelda6rightId');
+        $('#pelda6leftId').addClass('pelda6leftId');
+        $('#large-pelda7rightId').addClass('large-pelda7rightId');
+        $('#pelda7leftId').addClass('pelda7leftId');
+    } else {
+        //add class
+        $('#large-pelda1rightId').removeClass('large-pelda1rightId');
+        $('#pelda1leftId').removeClass('pelda1leftId');
+        $('#large-pelda2rightId').removeClass('large-pelda2rightId');
+        $('#pelda2leftId').removeClass('pelda2leftId');
+        $('#pelda3leftId').removeClass('pelda3leftId');
+        $('#large-pelda3rightId').removeClass('large-pelda3rightId');
+        $('#pelda4leftId').removeClass('pelda4leftId');
+        $('#large-pelda4rightId').removeClass('large-pelda4rightId');
+        $('#large-pelda5rightId').removeClass('large-pelda5rightId');
+        $('#pelda5leftId').removeClass('pelda5leftId');
+        $('#large-pelda6rightId').removeClass('large-pelda6rightId');
+        $('#pelda6leftId').removeClass('pelda6leftId');
+        $('#large-pelda7rightId').removeClass('large-pelda7rightId');
+        $('#pelda7leftId').removeClass('pelda7leftId');
+        //remove class
+        $('#large-pelda1rightId').addClass('medium-large-pelda1rightId');
+        $('#pelda1leftId').addClass('medium-pelda1leftId');
+        $('#large-pelda2rightId').addClass('medium-large-pelda2rightId');
+        $('#pelda2leftId').addClass('medium-pelda2leftId');
+        $('#pelda3leftId').addClass('medium-pelda3leftId');
+        $('#large-pelda3rightId').addClass('medium-large-pelda3rightId');
+        $('#pelda4leftId').addClass('medium-pelda4leftId');
+        $('#large-pelda4rightId').addClass('medium-large-pelda4rightId');
+        $('#large-pelda5rightId').addClass('medium-large-pelda5rightId');
+        $('#pelda5leftId').addClass('medium-pelda5leftId');
+        $('#large-pelda6rightId').addClass('medium-large-pelda6rightId');
+        $('#pelda6leftId').addClass('medium-pelda6leftId');
+        $('#large-pelda7rightId').addClass('medium-large-pelda7rightId');
+        $('#pelda7leftId').addClass('medium-pelda7leftId');
+    }
+
+    //multiscroll engine
+    $('#containerDivID').multiscroll({
+        anchors: ['cover3', 'smarthome3', 'automation3', 'green3', 'safety', 'security', 'innovation', 'tuya', 'pelda1', 'pelda2', 'pelda3', 'pelda5', 'pelda4', 'pelda7', 'pelda6'],
+        menu: '#menu',
+        navigation: true,
+        navigationTooltips: ['Borító', 'Intelligens épület', 'Automatizálás', 'Energia hatékonyság', 'Épületbiztonság', 'Vagyonbiztonság', 'Innovatív megoldások', 'Vezérlés', 'Reggeli Rutinok', 'Világításvezérlés és automatizálás', 'Fűtés-/Hűtésvezérlés', 'Vízérzékelés', 'Gázérzékelés', 'Egy biztonsági rendszer', 'Fagymentesítés'],
         verticalCentered: true,
-        scrollingSpeed: 700,
-        easing: 'easeInQuart',
-        easingcss3: 'ease-out',
-        menu: false,
-        sectionsColor: [],
-        navigation: false,
-        navigationPosition: 'right',
-        navigationColor: '#ffffff',
-        navigationTooltips: ['first', 'second', 'third', 'forth', 'fifth', 'sixth'],
-        loopBottom: false,
-        loopTop: false,
-        css3: true,
-        paddingTop: 0,
-        paddingBottom: 0,
-        normalScrollElements: null,
-        scrollOverflow: true,
-        scrollOverflowOptions: null,
-        keyboardScrolling: true,
-        touchSensitivity: 5,
-        //responsive
-        responsiveWidth: 0,
-        responsiveHeight: 0,
-        responsiveExpand: false,
 
-        // Custom selectors
-        sectionSelector: '.ms-section',
-        leftSelector: '.ms-left',
-        rightSelector: '.ms-right',
+        scrollingSpeed: 1000
 
-        //events
-        onLeave: function (index, nextIndex, direction) { },
-        afterLoad: function (anchorLink, index, active) { },
-        afterRender: function () { },
-        afterResize: function () { },
     });
+
+
 });
+
+$(window).on('resize', function () {
+    location.reload();
+})
