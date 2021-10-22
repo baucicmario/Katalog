3c1b10968d87fe7b74090420b5c3c0e7d25646f5
baucicmario <baucicmario2.6@gmail.com>
2021-10-22 15:37:24 +0200

    mobile view scaling fix

--- resources/js/multiscroll.js
+++ resources/js/multiscroll.js
@@ -1,25 +1,11 @@
 
 $(document).ready(function () {
     //small screen right side removal
-    if ($(window).width() > $(window).height()) {
 
-        $('#mobile-left').addClass('d-none');
-        $('#mobile-left').removeClass('ms-left');
-        $('#mobile-right').addClass('d-none');
-        $('#mobile-right').removeClass('ms-right');
-
-
-        $('#large-left').removeClass('d-none');
-        $('#large-left').addClass('ms-left');
-        $('#large-right').removeClass('d-none');
-        $('#large-right').addClass('ms-right');
-        $('#wifi-logo').removeClass('d-none');
-        $('#tuya-logo').removeClass('d-none');
-        $('#zigbee-logo').removeClass('d-none');
 
 
      //medium screen scaling
-     if ($(window).width() > $(window).height() * 1.8) {
+     if ($(window).width() > $(window).height() * 1.85) {
         $('.ms-section > .row').removeClass('bgcontain');
         $('#pelda6leftId > div').removeClass('bgcontain');
         $('.ms-section > .row').addClass('bgcover');
@@ -32,17 +18,35 @@ $(document).ready(function () {
         $('#pelda6leftId > div').removeClass('bgcover');
     }
     //multiscroll engine
-    $('#containerDivID').multiscroll({
-        anchors: ['cover', 'smarthome', 'automation', 'green', 'safety', 'security', 'innovation', 'tuya', 'pelda1', 'pelda2', 'pelda3', 'pelda5', 'pelda4', 'pelda7', 'pelda6', 'pelda8', 'large-empty', 'large-empty1', 'large-empty2', 'large-empty3', 'large-empty4', 'large-empty5', 'large-empty6'],
-        menu: '#menu',
-        navigationPosition: 'left',
-        navigation: false,
-        navigationTooltips: ['Borító', 'Intelligens épület', 'Automatizálás', 'Energia hatékonyság', 'Épületbiztonság', 'Vagyonbiztonság', 'Innovatív megoldások', 'Vezérlés', 'Reggeli Rutinok', 'Világításvezérlés és automatizálás', 'Fűtés-/Hűtésvezérlés', 'Vízérzékelés', 'Gázérzékelés', 'Egy biztonsági rendszer', 'Fagymentesítés'],
-        verticalCentered: true,
-        navigationColor: '#FFFFFF',
 
 
-    });
+
+
+    if ($(window).width() > $(window).height()) {
+
+        $('#mobile-left').addClass('d-none');
+        $('#mobile-left').removeClass('ms-left');
+        $('#mobile-right').addClass('d-none');
+        $('#mobile-right').removeClass('ms-right');
+
+
+        $('#large-left').removeClass('d-none');
+        $('#large-left').addClass('ms-left');
+        $('#large-right').removeClass('d-none');
+        $('#large-right').addClass('ms-right');
+
+        $('#containerDivID').multiscroll({
+            anchors: ['cover', 'smarthome', 'automation', 'green', 'safety', 'security', 'innovation', 'tuya', 'pelda1', 'pelda2', 'pelda3', 'pelda5', 'pelda4', 'pelda7', 'pelda6', 'pelda8', 'large-empty', 'large-empty1', 'large-empty2', 'large-empty3', 'large-empty4', 'large-empty5', 'large-empty6'],
+            menu: '#menu',
+            navigationPosition: 'left',
+            navigation: false,
+            navigationTooltips: ['Borító', 'Intelligens épület', 'Automatizálás', 'Energia hatékonyság', 'Épületbiztonság', 'Vagyonbiztonság', 'Innovatív megoldások', 'Vezérlés', 'Reggeli Rutinok', 'Világításvezérlés és automatizálás', 'Fűtés-/Hűtésvezérlés', 'Vízérzékelés', 'Gázérzékelés', 'Egy biztonsági rendszer', 'Fagymentesítés'],
+            verticalCentered: true,
+            navigationColor: '#FFFFFF',
+    
+    
+        });
+
 
     } else {
 
@@ -57,7 +61,33 @@ $(document).ready(function () {
         $('#mobile-right').removeClass('d-none');
         $('#mobile-right').addClass('ms-right');
     }
+
+
+    var oldwidth = $(window).width();
+    $(window).resize(function () {
+        var nw = $(window).width();
+        if(oldwidth>nw*1.1){
+            alert("I am smaller");
+            location.reload();
+
+        }else{
+            if(oldwidth*1.1<nw){
+                alert("I am bigger");
+                location.reload();
+            }
+        }       
+    });
+
+
+
+
+
+
+
+
 });
-$(window).on('resize', function () {
-    location.reload();
-})
+
+
+
+
+
