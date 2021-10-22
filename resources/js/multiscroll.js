2c2420060fd96770a53d346288b0350a6bb8ab57
baucicmario <baucicmario2.6@gmail.com>
2021-10-22 11:11:07 +0200

    halves on page fix

--- resources/js/multiscroll.js
+++ resources/js/multiscroll.js
@@ -35,70 +35,82 @@ $(document).ready(function () {
         $('#mobile-right').removeClass('d-none');
         $('#mobile-right').addClass('ms-right');
     }
-    //medium screen scaling
     if ($(window).width() > $(window).height() * 1.8) {
-        //remove class
-        $('#large-pelda1rightId').removeClass('medium-large-pelda1rightId');
-        $('#pelda1leftId').removeClass('medium-pelda1leftId');
-        $('#large-pelda2rightId').removeClass('medium-large-pelda2rightId');
-        $('#pelda2leftId').removeClass('medium-pelda2leftId');
-        $('#pelda3leftId').removeClass('medium-pelda3leftId');
-        $('#large-pelda3rightId').removeClass('medium-large-pelda3rightId');
-        $('#pelda4leftId').removeClass('medium-pelda4leftId');
-        $('#large-pelda4rightId').removeClass('medium-large-pelda4rightId');
-        $('#large-pelda5rightId').removeClass('medium-large-pelda5rightId');
-        $('#pelda5leftId').removeClass('medium-pelda5leftId');
-        $('#large-pelda6rightId').removeClass('medium-large-pelda6rightId');
-        $('#pelda6leftId').removeClass('medium-pelda6leftId');
-        $('#large-pelda7rightId').removeClass('medium-large-pelda7rightId');
-        $('#pelda7leftId').removeClass('medium-pelda7leftId');
-        //add classes
-        $('#large-pelda1rightId').addClass('large-pelda1rightId');
-        $('#pelda1leftId').addClass('pelda1leftId');
-        $('#large-pelda2rightId').addClass('large-pelda2rightId');
-        $('#pelda2leftId').addClass('pelda2leftId');
-        $('#pelda3leftId').addClass('pelda3leftId');
-        $('#large-pelda3rightId').addClass('large-pelda3rightId');
-        $('#pelda4leftId').addClass('pelda4leftId');
-        $('#large-pelda4rightId').addClass('large-pelda4rightId');
-        $('#large-pelda5rightId').addClass('large-pelda5rightId');
-        $('#pelda5leftId').addClass('pelda5leftId');
-        $('#large-pelda6rightId').addClass('large-pelda6rightId');
-        $('#pelda6leftId').addClass('pelda6leftId');
-        $('#large-pelda7rightId').addClass('large-pelda7rightId');
-        $('#pelda7leftId').addClass('pelda7leftId');
-    } else {
-        //add class
-        $('#large-pelda1rightId').removeClass('large-pelda1rightId');
-        $('#pelda1leftId').removeClass('pelda1leftId');
-        $('#large-pelda2rightId').removeClass('large-pelda2rightId');
-        $('#pelda2leftId').removeClass('pelda2leftId');
-        $('#pelda3leftId').removeClass('pelda3leftId');
-        $('#large-pelda3rightId').removeClass('large-pelda3rightId');
-        $('#pelda4leftId').removeClass('pelda4leftId');
-        $('#large-pelda4rightId').removeClass('large-pelda4rightId');
-        $('#large-pelda5rightId').removeClass('large-pelda5rightId');
-        $('#pelda5leftId').removeClass('pelda5leftId');
-        $('#large-pelda6rightId').removeClass('large-pelda6rightId');
-        $('#pelda6leftId').removeClass('pelda6leftId');
-        $('#large-pelda7rightId').removeClass('large-pelda7rightId');
-        $('#pelda7leftId').removeClass('pelda7leftId');
-        //remove class
-        $('#large-pelda1rightId').addClass('medium-large-pelda1rightId');
-        $('#pelda1leftId').addClass('medium-pelda1leftId');
-        $('#large-pelda2rightId').addClass('medium-large-pelda2rightId');
-        $('#pelda2leftId').addClass('medium-pelda2leftId');
-        $('#pelda3leftId').addClass('medium-pelda3leftId');
-        $('#large-pelda3rightId').addClass('medium-large-pelda3rightId');
-        $('#pelda4leftId').addClass('medium-pelda4leftId');
-        $('#large-pelda4rightId').addClass('medium-large-pelda4rightId');
-        $('#large-pelda5rightId').addClass('medium-large-pelda5rightId');
-        $('#pelda5leftId').addClass('medium-pelda5leftId');
-        $('#large-pelda6rightId').addClass('medium-large-pelda6rightId');
-        $('#pelda6leftId').addClass('medium-pelda6leftId');
-        $('#large-pelda7rightId').addClass('medium-large-pelda7rightId');
-        $('#pelda7leftId').addClass('medium-pelda7leftId');
+        $('.ms-section > .row').removeClass('bgcontain');
+        $('#pelda6leftId > div').removeClass('bgcontain');
+        $('.ms-section > .row').addClass('bgcover');
+        $('#pelda6leftId > div').addClass('bgcover');
+    } else{
+        
+        $('.ms-section > .row').addClass('bgcontain');
+        $('#pelda6leftId > div').addClass('bgcontain');
+        $('.ms-section > .row').removeClass('bgcover');
+        $('#pelda6leftId > div').removeClass('bgcover');
     }
+    //medium screen scaling
+    /* if ($(window).width() > $(window).height() * 1.8) {
+         //remove class
+         $('#large-pelda1rightId').removeClass('medium-large-pelda1rightId');
+         $('#pelda1leftId').removeClass('medium-pelda1leftId');
+         $('#large-pelda2rightId').removeClass('medium-large-pelda2rightId');
+         $('#pelda2leftId').removeClass('medium-pelda2leftId');
+         $('#pelda3leftId').removeClass('medium-pelda3leftId');
+         $('#large-pelda3rightId').removeClass('medium-large-pelda3rightId');
+         $('#pelda4leftId').removeClass('medium-pelda4leftId');
+         $('#large-pelda4rightId').removeClass('medium-large-pelda4rightId');
+         $('#large-pelda5rightId').removeClass('medium-large-pelda5rightId');
+         $('#pelda5leftId').removeClass('medium-pelda5leftId');
+         $('#large-pelda6rightId').removeClass('medium-large-pelda6rightId');
+         $('#pelda6leftId').removeClass('medium-pelda6leftId');
+         $('#large-pelda7rightId').removeClass('medium-large-pelda7rightId');
+         $('#pelda7leftId').removeClass('medium-pelda7leftId');
+         //add classes
+         $('#large-pelda1rightId').addClass('large-pelda1rightId');
+         $('#pelda1leftId').addClass('pelda1leftId');
+         $('#large-pelda2rightId').addClass('large-pelda2rightId');
+         $('#pelda2leftId').addClass('pelda2leftId');
+         $('#pelda3leftId').addClass('pelda3leftId');
+         $('#large-pelda3rightId').addClass('large-pelda3rightId');
+         $('#pelda4leftId').addClass('pelda4leftId');
+         $('#large-pelda4rightId').addClass('large-pelda4rightId');
+         $('#large-pelda5rightId').addClass('large-pelda5rightId');
+         $('#pelda5leftId').addClass('pelda5leftId');
+         $('#large-pelda6rightId').addClass('large-pelda6rightId');
+         $('#pelda6leftId').addClass('pelda6leftId');
+         $('#large-pelda7rightId').addClass('large-pelda7rightId');
+         $('#pelda7leftId').addClass('pelda7leftId');
+     } else {
+         //add class
+         $('#large-pelda1rightId').removeClass('large-pelda1rightId');
+         $('#pelda1leftId').removeClass('pelda1leftId');
+         $('#large-pelda2rightId').removeClass('large-pelda2rightId');
+         $('#pelda2leftId').removeClass('pelda2leftId');
+         $('#pelda3leftId').removeClass('pelda3leftId');
+         $('#large-pelda3rightId').removeClass('large-pelda3rightId');
+         $('#pelda4leftId').removeClass('pelda4leftId');
+         $('#large-pelda4rightId').removeClass('large-pelda4rightId');
+         $('#large-pelda5rightId').removeClass('large-pelda5rightId');
+         $('#pelda5leftId').removeClass('pelda5leftId');
+         $('#large-pelda6rightId').removeClass('large-pelda6rightId');
+         $('#pelda6leftId').removeClass('pelda6leftId');
+         $('#large-pelda7rightId').removeClass('large-pelda7rightId');
+         $('#pelda7leftId').removeClass('pelda7leftId');
+         //remove class
+         $('#large-pelda1rightId').addClass('medium-large-pelda1rightId');
+         $('#pelda1leftId').addClass('medium-pelda1leftId');
+         $('#large-pelda2rightId').addClass('medium-large-pelda2rightId');
+         $('#pelda2leftId').addClass('medium-pelda2leftId');
+         $('#pelda3leftId').addClass('medium-pelda3leftId');
+         $('#large-pelda3rightId').addClass('medium-large-pelda3rightId');
+         $('#pelda4leftId').addClass('medium-pelda4leftId');
+         $('#large-pelda4rightId').addClass('medium-large-pelda4rightId');
+         $('#large-pelda5rightId').addClass('medium-large-pelda5rightId');
+         $('#pelda5leftId').addClass('medium-pelda5leftId');
+         $('#large-pelda6rightId').addClass('medium-large-pelda6rightId');
+         $('#pelda6leftId').addClass('medium-pelda6leftId');
+         $('#large-pelda7rightId').addClass('medium-large-pelda7rightId');
+         $('#pelda7leftId').addClass('medium-pelda7leftId');
+     }*/
     //multiscroll engine
     $('#containerDivID').multiscroll({
         anchors: ['cover', 'smarthome', 'automation', 'green', 'safety', 'security', 'innovation', 'tuya', 'pelda1', 'pelda2', 'pelda3', 'pelda5', 'pelda4', 'pelda7', 'pelda6', 'pelda8', 'large-empty', 'large-empty1', 'large-empty2', 'large-empty3', 'large-empty4', 'large-empty5', 'large-empty6'],
