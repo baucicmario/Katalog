09a2fee2fc9c7fe29d1bd71b95c54d6fc60c95ae
baucicmario <baucicmario2.6@gmail.com>
2021-11-03 13:25:38 +0100

    everythong on one page

--- resources/js/multiscroll.js
+++ resources/js/multiscroll.js
@@ -3,6 +3,10 @@ $(document).ready(function() {
     //redundant classes
     $('#containerDivID > div > section').addClass('ms-section');
     $('#containerDivID > div > section').addClass('container-fluid');
+    $('#containerDivID > div > section > div').addClass('print2');
+    
+    $('#containerDivID > div > section ').addClass('print');
+    $('#containerDivID > div > section ').addClass('align-items-center');
     $('#containerDivID > div > section > div').addClass('row');
     $('#mobile-left > section').addClass('pt-5');
     $('#mobile-left > section > div').addClass('container-fluid');
@@ -49,29 +53,22 @@ $(document).ready(function() {
         $('#mobile-left > section').addClass('pt-5');
 
         //multiscroll engine
-        $('#containerDivID').multiscroll({
-            anchors: ['cover', 'smarthome', 'automation', 'camera', 'green', 'safety', 'security', 'innovation', 'wall', 'tuya', 'pelda1', 'pelda2', 'pelda3', 'pelda4', 'pelda5', 'pelda6', 'pelda7', 'pelda8', 'pelda9', 'katalog'],
-            menu: '#menu',
-            navigationPosition: 'left',
-            navigation: false,
-            navigationTooltips: ['Borító', 'Intelligens épület', 'Automatizálás', 'Kamera integráció', 'Energia hatékonyság', 'Épületbiztonság', 'Vagyonbiztonság', 'Innovatív megoldások', 'Vezérlés', 'Reggeli Rutinok', 'Világításvezérlés és automatizálás', 'Fűtés-/Hűtésvezérlés', 'Vízérzékelés', 'Gázérzékelés', 'Egy biztonsági rendszer', 'Fagymentesítés'],
-            verticalCentered: true,
-            navigationColor: '#FFFFFF',
-        });
+
     } else {
         //small screen
-        $('#left').addClass('d-none');
-        $('#left').removeClass('ms-left');
-        $('#right').addClass('d-none');
-        $('#right').removeClass('ms-right');
+        $('#mobile-left').addClass('d-none');
+        $('#mobile-left').removeClass('ms-left');
+        $('#mobile-right').addClass('d-none');
+        $('#mobile-right').removeClass('ms-right');
+        $('#left').removeClass('d-none');
+        $('#left').addClass('ms-left');
+        $('#right').removeClass('d-none');
+        $('#right').addClass('ms-right');
+        $('#menu2').addClass('d-none');
+        $('#menu').removeClass('d-none');
+        $('.ms-section').addClass('container-fluid');
+        $('#mobile-left > section').addClass('pt-5');
 
-
-        $('#mobile-left').removeClass('d-none');
-        $('#mobile-left').addClass('ms-left');
-        $('#mobile-right').removeClass('d-none');
-        $('#mobile-right').addClass('ms-right');
-        $('#menu2').removeClass('d-none');
-        $('#menu').addClass('d-none');
     }
 
 
