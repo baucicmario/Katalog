f1e77c8b1e8bf7d43564e0a8885a475c8026a6b2
baucicmario <baucicmario2.6@gmail.com>
2022-01-04 09:33:35 +0100

    Revert "Merge branch 'main' into For-Print"
    This reverts commit dda3658b6a28dc8525924d71e67e003d11fd3e43, reversing
    changes made to 43047eb5cc34523ccfeacb7864465ba48cf9b94a.

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,6 @@ import '../css/style.css'
 import '../css/sections.css'
 import '../css/pulse.css'
 import '../css/button.css'
-import '../css/scrollbar.css'
 import '../scss/main.scss';
 import '../scss/mobile_scroll.scss';
 import '../scss/intro.scss';
@@ -14,8 +13,8 @@ import '../scss/coverload.scss';
 import 'jquery/dist/jquery.min';
 import 'bootstrap/dist/js/bootstrap.min';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import 'three/build/three.min';
-//import 'vanta/src/vanta.waves';
+import 'three/build/three.min';
+import 'vanta/src/vanta.waves';
 
 import 'multiscroll.js/dist/jquery.multiscroll.min';
 import 'multiscroll.js/dist/jquery.multiscroll.min.css';
@@ -29,4 +28,4 @@ import './carosel';
 import '../css/sections.css'
 //import { effect } from "./vanta_effect";
 
-//effect;
+effect;
